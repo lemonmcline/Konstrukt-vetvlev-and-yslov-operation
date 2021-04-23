63e9d5f25c0c4cf98d7d189fe4f47b2a20fa2060
Artyom Pishalnikov <82143842+lemonmcline@users.noreply.github.com>
2021-04-23 12:20:52 +0600

    Konstrukt-vetvlev-and-yslov-operation
    Konstrukt-vetvlev-and-yslov-operation

---
+++ Konstrukt vetvlev and yslov operation.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main()
+	{
+	int height;
+	cin >> height;
+	if (height < 0) {
+		cout << "error";
+	}
+
+	else {
+
+		if (height > 170 && height < 300) {
+			if (height == 180) {
+				cout << "height ==180";
+			}
+			else {
+				cout << "height > 170" << endl;
+			}
+
+		}
+		else if (height < 170 && height < 300) {
+			cout << "height < 170";
+		}
+		else {
+			cout << "error";
+		}
+	}
+}
